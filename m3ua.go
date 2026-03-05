2fcdbf124b5524cd3ea1822991f245336adbe4ce
Tahseen Jamal <tahseen.jamal@gmail.com>
2026-03-05 12:05:14 +0530

    Updates

--- m3ua.go
+++ m3ua.go
@@ -2,30 +2,50 @@ package main
 
 import "encoding/binary"
 
+const (
+	M3UA_VERSION = 1
+	M3UA_HEADER  = 8
+)
+
 type M3UAMessage struct {
+	Version uint8
 	Class   uint8
 	Type    uint8
+	Length  uint32
 	Payload []byte
 }
 
 func ParseM3UA(data []byte) (M3UAMessage, bool) {
 
-	if len(data) < 8 {
+	// Minimum header length
+	if len(data) < M3UA_HEADER {
 		return M3UAMessage{}, false
 	}
 
-	length := int(binary.BigEndian.Uint32(data[4:8]))
+	version := data[0]
 
-	if length < 8 || length > len(data) {
+	// Validate version
+	if version != M3UA_VERSION {
+		return M3UAMessage{}, false
+	}
+
+	msgClass := data[2]
+	msgType := data[3]
+
+	length := binary.BigEndian.Uint32(data[4:8])
+
+	// Length validation
+	if length < M3UA_HEADER || int(length) > len(data) {
 		return M3UAMessage{}, false
 	}
 
 	msg := M3UAMessage{
-		Class: data[2],
-		Type:  data[3],
+		Version: version,
+		Class:   msgClass,
+		Type:    msgType,
+		Length:  length,
+		Payload: data[M3UA_HEADER:length],
 	}
-
-	msg.Payload = data[8:length]
 
 	return msg, true
 }
